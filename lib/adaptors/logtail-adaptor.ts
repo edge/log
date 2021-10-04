f0100de69ee93aafb331819c24436acf0f0e5de7
Adam K Dean <adamkdean@googlemail.com>
2021-10-04 21:45:14 +0100

    feat: allow disabling of name injection in LogtailAdaptor

--- lib/adaptors/logtail-adaptor.ts
+++ lib/adaptors/logtail-adaptor.ts
@@ -8,29 +8,33 @@ import { Adaptor, Log } from '..'
 
 export class LogtailAdaptor implements Adaptor {
   private logtail: Logtail
+  private enableNameInjection: boolean
 
-  constructor(logtailSourceToken: string) {
+  constructor(logtailSourceToken: string, enableNameInjection = true) {
     this.logtail = new Logtail(logtailSourceToken)
+    this.enableNameInjection = enableNameInjection
   }
 
   debug(log: Log, message: string, context?: Record<string, unknown>): void {
-    this.logtail.debug(this.format(message, log.name), this.addNameToContext(log, context))
+    this.logtail.debug(this.format(message, log.name), this.injectNameIntoContext(log, context))
   }
 
   info(log: Log, message: string, context?: Record<string, unknown>): void {
-    this.logtail.info(this.format(message, log.name), this.addNameToContext(log, context))
+    this.logtail.info(this.format(message, log.name), this.injectNameIntoContext(log, context))
   }
 
   warn(log: Log, message: string, context?: Record<string, unknown>): void {
-    this.logtail.warn(this.format(message, log.name), this.addNameToContext(log, context))
+    this.logtail.warn(this.format(message, log.name), this.injectNameIntoContext(log, context))
   }
 
   error(log: Log, message: string, context?: Record<string, unknown>): void {
-    this.logtail.error(this.format(message, log.name), this.addNameToContext(log, context))
+    this.logtail.error(this.format(message, log.name), this.injectNameIntoContext(log, context))
   }
 
-  private addNameToContext(log: Log, context?: Record<string, unknown>): Context {
-    return { name: log.name, ...context } as Context
+  private injectNameIntoContext(log: Log, context?: Record<string, unknown>): Context {
+    return this.enableNameInjection
+      ? { name: log.name, ...context } as Context
+      : context as Context
   }
 
   private format(message: string, name?: string): string {
