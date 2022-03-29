d0faaf1b49c8e457b401be0a4745604d7c5ac585
Aneurin Barker Snook <a@aneur.in>
2022-03-29 16:53:35 +0100

    fix: condense elastic adaptor
    incl removing backup logger - just use multi adaptor

--- lib/adaptors/elastic-adaptor.ts
+++ lib/adaptors/elastic-adaptor.ts
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a GNU GPL-style license
 // that can be found in the LICENSE.md file. All rights reserved.
 
-import { StdioAdaptor } from './stdio-adaptor'
 import superagent from 'superagent'
 import { Adaptor, Log } from '..'
 
@@ -22,7 +21,6 @@ export type Options = {
 }
 
 export class ElasticAdaptor implements Adaptor {
-  private backup: Adaptor
   private config: ElasticConfig
   private options: Options
 
@@ -30,7 +28,6 @@ export class ElasticAdaptor implements Adaptor {
     if (!config.apiKey && !config.username) {
       throw new Error('API key or username/password required')
     }
-    this.backup = new StdioAdaptor()
     this.config = config
     this.options = options || {}
   }
@@ -51,53 +48,33 @@ export class ElasticAdaptor implements Adaptor {
     this.send(log, 'error', message, context)
   }
 
-  private prepareData(
-    name: string | undefined,
-    level: string,
-    message: string,
-    context?: Record<string, unknown>
-  ): Record<string, unknown> {
+  private async send(log: Log, level: string, message: string, context?: Record<string, unknown>): Promise<void> {
     const timestamp = (new Date()).toISOString()
-    return {
+    const data = {
       '@timestamp': timestamp,
-      name,
+      name: log.name,
       level,
       message,
       context,
       ...this.options
     }
-  }
 
-  private async send(log: Log, level: string, message: string, context?: Record<string, unknown>): Promise<void> {
     try {
-      const req = superagent.post(`${this.config.host}/${this.config.dataStream}/_doc`)
-        .send(this.prepareData(log.name, level, message, context))
+      const req = superagent.post(`${this.config.host}/${this.config.dataStream}/_doc`).send(data)
 
       if (this.config.apiKey) req.set('Authorization', `apikey ${this.config.apiKey}`)
       else {
         const auth = Buffer.from(`${this.config.username}:${this.config.password}`, 'utf-8').toString('base64')
         req.set('Authorization', `basic ${auth}`)
       }
+
       if (this.config.cert) req.ca(this.config.cert)
       else if (this.config.cert === false) req.disableTLSCerts()
+
       await req
     }
     catch (err) {
-      this.backup.error(log, 'Failed to send log to Elasticsearch', { err })
-      switch (level) {
-      case 'debug':
-        this.backup.debug(log, message, context)
-        break
-      case 'info':
-        this.backup.info(log, message, context)
-        break
-      case 'warn':
-        this.backup.warn(log, message, context)
-        break
-      case 'error':
-        this.backup.error(log, message, context)
-        break
-      }
+      console.error(err)
     }
   }
 }
